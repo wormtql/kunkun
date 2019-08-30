cf94482cb0c4d8fdedf5028120c898fafdae0028
IATOW <584130248@qq.com>
2019-08-30 14:40:46 +0800

    add more api specs

--- ClientUtils.h
+++ ClientUtils.h
@@ -10,7 +10,20 @@ class ClientUtils {
 private:
 
 public:
+
     static json login_blocked(const std::string & username, const std::string & password);
+
+
+    static json signup_blocked(const std::string & username, const std::string & password);
+
+
+    static json create_group_blocked(const std::string & who, const std::string & group_name);
+
+
+    static json rename_group_blocked(int group_id, const std::string & new_name, const std::string who);
+
+
+    // todo ...
 };
 
 
